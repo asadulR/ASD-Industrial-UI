aa0427b09a1569adc9f2f735408cd56806f8f45f
asadulR <asad1252001@gmail.com.com>
2022-05-22 19:55:56 +0600

    footer added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import Footer from '../../components/Footer';
 import ExpertTeam from './ExpertTeam';
 import Landing from './Landing';
 import Products from './Products';
 import WeProvide from './WeProvide';
 
 const Home = () => {
+    
     return (
         <main className='container mx-auto'>
             <Landing />
             <WeProvide></WeProvide>
             <Products />
             <ExpertTeam />
+
+            <Footer/>
         </main>
     );
 };
